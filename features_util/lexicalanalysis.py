17aa8ce15d56a0792e3a3e478ba208716942c226
taimoorkhan-nlp <taimoor.nlp@gmail.com>
2025-11-11 14:48:24 +0100

    updated

---
+++ features_util/lexicalanalysis.py
@@ -0,0 +1,35 @@
+from lexicalrichness import LexicalRichness
+
+#Returns the list of UNIQUE Words present in a text.
+def get_tokens(text):
+    return LexicalRichness(text).terms
+
+#TTR: Total number of different words / Total number of words (tokens). It depends on the length of the text.
+def get_ttr(text):
+    return LexicalRichness(text).ttr
+
+#rTTR: Number of unique words / Square root of the total number of words. 
+#To provide a more consistent measure of lexical diversity across texts of varying lengths.
+def get_rttr(text):
+    return LexicalRichness(text).rttr
+
+#cTTR is less sensitive to text length and might be preferable due to its more stringent adjustment.
+def get_cttr(text):
+    return LexicalRichness(text).cttr
+
+#Logarithmic and Normalized. Herdan's C (C=logV/logN) 
+#High C Value: Indicates high lexical diversity, meaning the text uses a wide range of different words relative to its length.
+def get_herdan(text):
+    return LexicalRichness(text).Herdan
+
+#Logarithmic measure independent of text length. Maas' T decreases as lexical richness increases.
+def get_maas(text):
+    return LexicalRichness(text).Maas
+
+#Probability-based measure of encountering different types of words. Yule's K decreases as lexical richness increases.
+def get_yulei(text):
+    try:
+        return LexicalRichness(text).yulei
+    except ValueError as e:
+        return None
+    
---
+++ features_util/readability.py
@@ -0,0 +1,35 @@
+import textstat
+
+#Returns the Flesch Reading Ease Score. 
+#While the maximum score is 121.22, there is no limit on how low the score can be. A negative score is valid.
+def get_flesch_reading_ease(text):
+    return textstat.flesch_reading_ease(text)
+
+#Based upon all the possible tests, returns the estimated school grade level required to understand the text.
+def get_text_standard(text):
+    return textstat.text_standard(text, float_output=False)
+
+#Returns the reading time of the given text.
+#Assumes 14.69ms per character.
+def get_reading_time(text):
+    return textstat.reading_time(text, ms_per_char=14.69)
+
+#Returns the number of syllables present in the given text.
+def get_syllable_count(text):
+    return textstat.syllable_count(text)
+
+#Returns the number of sentences present in the given text.
+def get_sentence_count(text):
+    return textstat.sentence_count(text)
+
+#Returns the number of characters present in the given text.
+def get_char_count(text):
+    return textstat.char_count(text, ignore_spaces=True)
+
+#Returns the number of words with a syllable count greater than or equal to 3.
+def get_polysyllable_count(text):
+    return textstat.polysyllabcount(text)
+
+#Returns the number of words with a syllable count equal to one.
+def get_monosyllable_count(text):
+    return textstat.monosyllabcount(text)
